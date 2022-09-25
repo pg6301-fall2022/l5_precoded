103c3de5bf3dc6a7d05ec95a2690cb454c8580ab
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-09-25 21:32:49 +0200

    Step 5 - post with some contents

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
+
+const users = [
+    {
+        username: "admin", password: "mostly secret"
+    }
+]
+
+
 
 app.get("/login", (req, res) => {
     // GET returns some data ( server -> client)
@@ -11,8 +21,19 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
     // POST sends data (client -> server)
-    console.log("Test");
-    res.end();
+    // Since POST sends data from client, we can use it to check that password is correct
+
+    const body = req.body;
+    const username = body.username;
+    const password = body.password;
+
+    if(users.find(u => u.username === username).password === password) {
+        res.sendStatus(200);
+    }
+    else {
+        res.send(401);
+    }
+
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
