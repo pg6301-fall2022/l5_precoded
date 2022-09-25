df09012de527593a8124ba4cb1738f747d3a32db
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2022-09-25 21:24:16 +0200

    Step 4 - (very) basic post

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ import express from "express";
 const app = express();
 
 app.get("/login", (req, res) => {
+    // GET returns some data ( server -> client)
     res.json({
         username: "admin"
     });
+});
+
+app.post("/login", (req, res) => {
+    // POST sends data (client -> server)
+    console.log("Test");
+    res.end();
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
